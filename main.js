00b0800b3b4c0dad55d26d6dfb5524ededdf27e3
Jacob Richards <jacob.richards33@gmail.com>
2019-05-22 22:23:21 -0700

    repl and google iframes

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 let background = document.createElement('div');
 background.className = 'blackbg';
 background.innerHTML = `
-  <div class="text-container">
+  <iframe frameborder="0" width="100%" height="500px" src="https://repl.it/@richardsjacob/DifferentAquaQuerylanguage?lite=true"></iframe>
+  <iframe frameborder="0" width="100%" height="500px" style="padding-top: 20px" src="https://www.google.com/webhp?igu=1"></iframe>  
+
+<div class="text-container">
     <form class="form">
       <input type="text" class="txt-input" placeholder="Hi, what\'s your name?"></input>
       <input type="submit" class="submit-txt"></input>
